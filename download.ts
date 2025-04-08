3f02e6226e53121791a339c4e3a28118e2131ad5
Gordon Leigh <gordon@propulsionworks.io>
2025-04-08 08:41:30 +0200

    enhance schema documentation with @aws-cdk/aws-service-spec

--- download.ts
+++ download.ts
@@ -1,3 +1,9 @@
+import { loadAwsServiceSpec } from "@aws-cdk/aws-service-spec";
+import {
+  RichSpecDatabase,
+  type Resource,
+  type TypeDefinition,
+} from "@aws-cdk/service-spec-types";
 import canonicalize from "canonicalize";
 import assert from "node:assert";
 import { randomUUID } from "node:crypto";
@@ -11,7 +17,12 @@ import { pipeline } from "node:stream/promises";
 import { createDeflate, createGunzip } from "node:zlib";
 import { fromData } from "ssri";
 import { ZipReader } from "zip24/reader";
-import type { ResourceTypeSchema } from "./src/types.ts";
+import type { PropertySchema, ResourceTypeSchema } from "./src/types.ts";
+import { getDocumentationUrl as makeAwsDocumentationUrl } from "./src/util.ts";
+
+type SchemaWithDocUrl = PropertySchema & {
+  documentationUrl?: string | undefined;
+};
 
 const SchemaOutputPath = resolve(import.meta.dirname, "./schemas");
 
@@ -48,11 +59,38 @@ const tempPath = join(tmpdir(), randomUUID() + ".zip");
 const output = createWriteStream(tempPath);
 await pipeline(result, output);
 
+// aws data source for better documentation
+const db = new RichSpecDatabase(await loadAwsServiceSpec());
+
 await mkdir(SchemaOutputPath, { recursive: true });
 const zip = await ZipReader.open(tempPath);
 
 for await (const entry of zip) {
   const schema = JSON.parse(await entry.toText()) as ResourceTypeSchema;
+
+  let resource: Resource | undefined;
+  let definitions: readonly TypeDefinition[];
+
+  try {
+    resource = db.resourceByType(schema.typeName);
+    definitions = db.resourceTypeDefs(schema.typeName);
+  } catch {
+    // there is no specific error type to catch so assume it's be cause the type
+    // is not found
+    definitions = [];
+  }
+
+  if (resource) {
+    enhanceSchema(schema.typeName, schema, undefined, resource);
+  }
+  if (schema.definitions) {
+    for (const [defName, defSchema] of Object.entries(schema.definitions)) {
+      const def = definitions.find((x) => x.name === defName);
+      if (def) {
+        enhanceSchema(schema.typeName, defSchema, defName, def);
+      }
+    }
+  }
 
   const canonicalJson = canonicalize(schema);
   assert(canonicalJson);
@@ -70,4 +108,45 @@ for await (const entry of zip) {
     join(SchemaOutputPath, entry.path),
     JSON.stringify(sortedSchema, undefined, 2)
   );
+}
+
+function enhanceSchema(
+  typeName: string,
+  schema: SchemaWithDocUrl | ResourceTypeSchema,
+  definitionName: string | undefined,
+  definition: Resource | TypeDefinition
+): void {
+  if (definition.documentation) {
+    schema.description = definition.documentation;
+    schema.documentationUrl ??= makeAwsDocumentationUrl(
+      typeName,
+      definitionName
+    );
+  }
+  if (!schema.description?.includes(typeName)) {
+    schema.description = lines(
+      definitionName
+        ? `Type definition for \`${typeName}.${definitionName}\`.`
+        : `Resource type definition for \`${typeName}\`.`,
+      schema.description
+    );
+  }
+  if (schema.properties) {
+    const props = Object.entries(schema.properties);
+    for (const [propertyName, propertySchema] of props) {
+      if (typeof propertySchema === "boolean") {
+        continue;
+      }
+      const prop = definition.properties[propertyName];
+      if (prop?.documentation) {
+        propertySchema.description = prop.documentation;
+      }
+      (propertySchema as SchemaWithDocUrl).documentationUrl =
+        makeAwsDocumentationUrl(typeName, definitionName, propertyName);
+    }
+  }
+}
+
+function lines(...lines: (string | undefined)[]): string {
+  return lines.filter(Boolean).join("\n");
 }
---
+++ src/util.ts
@@ -0,0 +1,40 @@
+import assert from "node:assert";
+
+export function getDocumentationUrl(
+  typeName: string,
+  definitionName?: string | null,
+  propertyName?: string
+): string | undefined {
+  const match = /^(?:aws|alexa)::(.+)::([^.]+)$/.exec(typeName.toLowerCase());
+  if (!match) {
+    return;
+  }
+
+  const service = match[1];
+  const resource = match[2];
+  assert(service && resource);
+
+  let page: string;
+  let fragment = "";
+
+  if (typeof definitionName === "string") {
+    page = `aws-properties-${service}-${resource}-${definitionName.toLowerCase()}`;
+  } else {
+    page = `aws-resource-${service}-${resource}`;
+  }
+
+  if (definitionName === null) {
+    // attributes
+    fragment = `#aws-resource-${service}-${resource}-return-values`;
+  } else if (propertyName !== undefined) {
+    if (typeof definitionName === "string") {
+      fragment = `#cfn-${service}-${resource}-${definitionName.toLowerCase()}-${propertyName.toLowerCase()}`;
+    } else {
+      fragment = `#cfn-${service}-${resource}-${propertyName.toLowerCase()}`;
+    }
+  }
+
+  // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-s3-bucket-accelerateconfiguration.html#cfn-s3-bucket-accelerateconfiguration-accelerationstatus
+
+  return `https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/${page}.html${fragment}`;
+}
